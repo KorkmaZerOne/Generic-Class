6bd38a1ab228f1a0f420db6238c92b0f902a8eb8
Omer <Omer@192.168.1.20>
2020-04-15 16:38:41 +0200

    was made some changes

--- Generieken_HoofdStuk_17/src/DuoApp.java
+++ Generieken_HoofdStuk_17/src/DuoApp.java
@@ -7,6 +7,9 @@ public class DuoApp {
 		Duo<Integer> id = new Duo(7 , 5);
 		Duo<Number> dn = new Duo<>(8 , 2);
 		
+		Duo<?> [] iduo = new Duo<?> [5];
+		
+		
 		String s1 = sd.getFirst();
 		String s2 = sd.getSecond();
 		
@@ -16,10 +19,6 @@ public class DuoApp {
 		System.out.println(s1 + " " + s2);
 		System.out.println(i1 + i2);
 		System.out.println(s1 + " " + i1);
-		
-		DuoUtility.printSum(dn);
-		DuoUtility.printSumEx(dn);
-
 	}
 
 }
--- Generieken_HoofdStuk_17/src/DuoUtilityApp.java
+++ Generieken_HoofdStuk_17/src/DuoUtilityApp.java
@@ -5,15 +5,29 @@ public class DuoUtilityApp {
 		
 		Duo<Integer> id = new Duo<>(7 , 5);
 		Duo<Long> 	 ld = new Duo<>(746987L , 546358L);
-		Duo<Float> 	 fd = new Duo<>(5.7F , 8.9F);
+		Duo<Number>  fd = new Duo<>(5.7F , 8.9F);
 		Duo<Number>  nd = new Duo<>(4 , 8.56D);
 		
-		DuoUtility.printSum(id);
-		DuoUtility.printSum(ld);
-		DuoUtility.printSum(fd);
-		DuoUtility.printSum(nd);
+		DuoUtility.printSum(id, ld);
+		DuoUtility.printSum(ld, fd);
+		DuoUtility.printSum(fd, nd);
+		DuoUtility.printSum(nd, id);
 		
+		DuoUtility.printSumSame(nd, fd);
 		
+		DuoUtility.swapFirst(fd, nd);
+		DuoUtility.printSum(nd, fd);
+		DuoUtility.printSum(fd, nd);
+		
+		Duo<String> d1 = new Duo<>("Hello" , "World");
+		Duo<String> d2 = new Duo<>("Goodbye" , "Mars");
+		
+		DuoUtility.printDuo(d1);
+		DuoUtility.printDuo(d2);
+		
+		DuoUtility.swapFirst(d1, d2);
+		DuoUtility.printDuo(d1);
+		DuoUtility.printDuo(d2);
 	}
 
 }
